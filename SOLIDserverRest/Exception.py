a9e44d1fb5a56c0f764487fccf8465e7a3ac37d9
Alex Chauvin <ach@meta-x.org>
2019-06-09 19:19:30 +0200

    add SSDAuthError

--- SOLIDserverRest/Exception.py
+++ SOLIDserverRest/Exception.py
@@ -8,12 +8,14 @@ Exceptions for the SOLIDServer modules
 __all__ = ["SSDError",
            "SSDInitError",
            "SSDServiceError",
-           "SSDRequestError"]
+           "SSDRequestError",
+           "SSDAuthError"]
 
 
 class SSDError(Exception):
     """ generic class for any exception in SOLIDServer communication """
     def __init__(self, message=""):
+        super(SSDError, self).__init__()
         self.message = message
 
     def __str__(self):
@@ -22,27 +24,36 @@ class SSDError(Exception):
 
 class SSDInitError(SSDError):
     """ raised when action on non initialized SSD connection """
-    pass
+    def __init__(self, message=""):
+        super(SSDInitError, self).__init__("[init] {}".format(message))
 
 
 class SSDServiceError(SSDError):
     """ raised on unknown service """
 
     def __init__(self, service_name, message=""):
+        super(SSDServiceError, self).__init__(message)
         self.service = service_name
-        self.message = message
 
     def __str__(self):
         return "{} on service {}".format(self.message, self.service)
+
 
 class SSDRequestError(SSDError):
     """ raised when urllib request is failing """
 
     def __init__(self, method, url, headers, message=""):
-        self.message = message
+        super(SSDRequestError, self).__init__(message)
         self.method = method
         self.url = url
         self.headers = headers
 
     def __str__(self):
         return "{} with {} {}".format(self.message, self.method, self.url)
+
+
+class SSDAuthError(SSDError):
+    """ raised when auth on request is wrong """
+
+    def __init__(self, message=""):
+        super(SSDAuthError, self).__init__("authent {}".format(message))
