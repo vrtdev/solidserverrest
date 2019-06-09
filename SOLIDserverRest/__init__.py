110535399c0eae4eb4d2c8888b7bbf8046f3ed64
Alex Chauvin <ach@meta-x.org>
2019-06-09 19:20:04 +0200

    lint

--- SOLIDserverRest/__init__.py
+++ SOLIDserverRest/__init__.py
@@ -1,4 +1,3 @@
-# from .Exception import *
 
 """
 init of the SOLIDserver module
@@ -7,9 +6,11 @@ init of the SOLIDserver module
 import sys
 
 if sys.version_info[0] == 2:
+    # pylint: disable=F0401, W0406
     from SOLIDserverRest import *
     from mapper import *
     from Exception import *
+    # pylint: enable=F0401, W0406
 else:
     from .mapper import *
     from .SOLIDserverRest import *
