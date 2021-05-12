1a70d1254c90f503c6c34533312e75284d31cf04
Alex Chauvin <ach@efficientip.com>
2021-05-12 08:36:20 +0200

    update setup for publish

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, 'README.md')) as f:
 
 setup(
     name='SOLIDserverRest',
-    version='2.1.8',
+    version='2.1.9',
     author='Gregory CUGAT / Alex Chauvin',
     url='https://gitlab.com/efficientip/solidserverrest',
     description='The SOLIDserverRest is a library to drive EfficientIP API',
